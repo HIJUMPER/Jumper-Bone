e1bdd20ccafd26b4c88cafdab55681fd7fea6adc
Jumper <szpzhang@cn.ibm.com>
2018-07-13 18:50:16 +0800

    Graceful Shutdown

---
+++ SingleNode/src/main/java/cn/jumper/bone/singlenode/SingleNodeApplication.java
@@ -0,0 +1,12 @@
+package cn.jumper.bone.singlenode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleNodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SingleNodeApplication.class, args);
+    }
+}
